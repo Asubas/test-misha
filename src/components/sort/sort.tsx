9fd8c4268cd6802257a8bf6a760e3e7c563c72df
Леха <znaxor74@mail.ru>
2025-02-18 03:31:54 +0300

    feat: implement function sorting

---
+++ src/components/sort/sort.tsx
@@ -0,0 +1,57 @@
+import "./sort.css";
+import { IProduct } from "../../types/productInterface";
+import { useState } from "react";
+
+export function Sort({
+  sortProducts,
+  prod,
+}: {
+  sortProducts: (product: IProduct[]) => void;
+  prod: IProduct[];
+}) {
+  const [sortOrder, setSortOrder] = useState<{
+    field: string | null;
+    direction: "asc" | "desc" | null;
+  }>({
+    field: null,
+    direction: null,
+  });
+
+  const sortByName = () => {
+    const direction =
+      sortOrder.field === "name" && sortOrder.direction === "asc"
+        ? "desc"
+        : "asc";
+    const sortArray = [...prod].sort((a, b) =>
+      direction === "asc"
+        ? a.name.localeCompare(b.name)
+        : b.name.localeCompare(a.name)
+    );
+    setSortOrder({ field: "name", direction });
+    sortProducts(sortArray);
+  };
+
+  const sortByPrice = () => {
+    const direction =
+      sortOrder.field === "price" && sortOrder.direction === "asc"
+        ? "desc"
+        : "asc";
+    const sortArray = [...prod].sort((a, b) =>
+      direction === "asc" ? a.price - b.price : b.price - a.price
+    );
+    setSortOrder({ field: "price", direction });
+    sortProducts(sortArray);
+  };
+
+  return (
+    <div className="sortWrapper">
+      <span>Сортировать по : </span>
+      <button className="sortButton" onClick={sortByName}>
+        Имя
+      </button>
+      <button className="sortButton" onClick={sortByPrice}>
+        Цена
+      </button>
+    </div>
+  );
+}
--- src/pages/element/element.tsx
+++ src/pages/element/element.tsx
@@ -4,21 +4,25 @@ import { getProducts } from "../../utils/getProducts";
 import { useEffect, useState } from "react";
 import { IProduct } from "../../types/productInterface";
 import { Pagination } from "../../components/pagination/pagination";
+import { Sort } from "../../components/sort/sort";
 
 export function Element() {
   const params = useParams();
   const currentProduct = params.element;
   const [products, setProducts] = useState<IProduct[]>([]);
   const [page, setCurrentPage] = useState(1);
+  const [sortedProducts, setSortedProducts] = useState<IProduct[]>([]);
   const startIndex = (page - 1) * 4;
   const endIndex = startIndex + 4;
-  const currentProducts = products.slice(startIndex, endIndex);
+  const currentProducts = sortedProducts.slice(startIndex, endIndex);
 
   useEffect(() => {
     if (currentProduct) {
       getProducts(currentProduct)
         .then(async (data) => {
           setProducts(data);
+          setSortedProducts(data);
+          setCurrentPage(1);
         })
         .catch((error) => {
           console.error("Ошибка при загрузке данных:", error);
@@ -30,6 +34,13 @@ export function Element() {
     <>
       {products.length > 0 ? (
         <div className="elementWrapper">
+          <Sort
+            prod={products}
+            sortProducts={(sorted: IProduct[]) => {
+              setSortedProducts(sorted);
+              setCurrentPage(1);
+            }}
+          />
           <ul className="elementList">
             {currentProducts.map((product) => (
               <li className="productItem" key={product.id}>
@@ -41,7 +52,7 @@ export function Element() {
           </ul>
           <Pagination
             currentPage={page}
-            totalPosts={products.length}
+            totalPosts={sortedProducts.length}
             paginate={(page: number) => setCurrentPage(page)}
           />
         </div>
